5ab78db59bbc16900d7e3f92cbb790cb1305000f
Austin Jackson <austinjckson@gmail.com>
2018-07-28 19:06:30 -0500

    update readme

--- README.md
+++ README.md
@@ -7,13 +7,31 @@ Telegram bot to act as a courier for Plex content requests.
 
 Hatsume is written in Node.js v10.x, however she might work on lower versions. Keep this in mind when deploying.
 
-To get started:
-- I'll write this later
-- Setup mySQL or MariaDB (MariaDB please)
+### Hatsume depends on the following APIs. You'll need to supply API keys from them:
+- https://core.telegram.org/bots/api#authorizing-your-bot
+- http://www.omdbapi.com/
+- https://api.thetvdb.com/swagger
+- https://www.themoviedb.org/documentation/api
+
+### To get started:
+- Clone repo and install dependencies:
+- `git clone https://github.com/au5ton/hatsume.git`
+- `npm install`
+- [Setup mySQL or MariaDB (MariaDB please)](https://www.digitalocean.com/community/tutorials/how-to-install-mariadb-on-centos-7)
 - Import the SQL database: 
-- `mysql -u root -p`
-- `mysql> CREATE DATABASE new_database;`
-- `mysql -u username -p new_database < bot_data.sql`
+- `mysql -u [username] -p newdatabase < sql/hatsume_db.sql`
+- Setup environment variables:
+- `cp .env.example .env`
+- `nano .env`
+- Run the bot:
+- `node bot.js`
+- If all startup checks pass, you're good to go!
+
+## Usage
+
+<img alt="screenshot1" src="img/screenshot1.png" width=480>
+<img alt="screenshot2" src="img/screenshot2.png" width=480>
+
 
 ##### Character
 
---
+++ sql/hatsume_db.sql
@@ -0,0 +1,84 @@
+-- MySQL dump 10.16  Distrib 10.1.26-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: hatsumebot
+-- ------------------------------------------------------
+-- Server version	10.1.26-MariaDB-0+deb9u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `requests`
+--
+
+DROP TABLE IF EXISTS `requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `requests` (
+  `request_id` int(11) NOT NULL AUTO_INCREMENT,
+  `telegram_id` int(11) DEFAULT NULL,
+  `imdb_id` varchar(20) DEFAULT NULL,
+  `tvdb_id` int(11) DEFAULT NULL,
+  `content_name` varchar(255) DEFAULT NULL,
+  `is_tv` tinyint(1) DEFAULT NULL,
+  `tmdb_id` int(11) DEFAULT NULL,
+  `network` varchar(50) DEFAULT NULL,
+  `start_year` int(11) DEFAULT NULL,
+  PRIMARY KEY (`request_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `requests`
+--
+
+LOCK TABLES `requests` WRITE;
+/*!40000 ALTER TABLE `requests` DISABLE KEYS */;
+INSERT INTO `requests` VALUES (1,0,'tt0000001',NULL,'Carmencita',0,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `requests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `telegram_id` int(11) DEFAULT NULL,
+  `telegram_name` varchar(40) DEFAULT NULL,
+  `telegram_handle` varchar(33) DEFAULT NULL,
+  `plex_username` varchar(50) DEFAULT NULL,
+  `chat_state` int(11) DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (0,'Pavel Durov','durov','durov@telegram.org',0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-07-28 23:13:55
